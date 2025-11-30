3d89fbe4f1e60171e3a6eb35b4785ee5296bf795
abarbarapeslo <abarbarapeslo@users.noreply.github.com>
2025-11-30 21:31:17 +0000

    deploy: d3478754ab29f4c437b83f4cc755bfbf2c607864

---
+++ assets/js/aba21aa0.b1aa2786.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhackathon_transpetro=self.webpackChunkhackathon_transpetro||[]).push([[742],{7093:a=>{a.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
